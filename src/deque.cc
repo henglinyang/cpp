e8ab4d0a928ca309a01340e8a10359455a258cb5
Henglin Yang <henglin@yahoo.com>
2021-12-10 22:00:29 -0500

    deque

---
+++ src/deque.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main()
+{
+  deque<int> coll;  // deque container for integers
+
+  for (int i=0; i<3; ++i) {
+    coll.push_front(i);
+  }
+  for (int i=0; i<3; ++i) {
+    coll.push_back(i + 100);
+  }
+
+  for (unsigned i=0; i<coll.size(); ++i) {
+    cout << coll[i] << ' ';
+  }
+  cout << endl;
+
+  return 0;
+}  
