3a56c03235c8d4c0ac1d91eaa5835475ac867ef8
Henglin Yang <henglin@yahoo.com>
2021-12-10 21:53:11 -0500

    vector

---
+++ src/vector.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void print(vector<int> coll)
+{
+  for (unsigned i=0; i<coll.size(); ++i) {
+    cout << coll[i] << ' ';
+  }
+  cout << endl;
+}
+
+int main()
+{
+  vector<int> coll; // interger vector container
+
+  for (int i=0; i<6; ++i) {
+    coll.push_back(i);
+  }
+
+  print(coll);
+  return 0;
+}
